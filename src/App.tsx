b91afc01ded12735f568f53e3fdcac7c078daf58
keenager <keenager84@gmail.com>
2026-08-20 12:15:59 -0700

    tauri 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import './App.css'
-import CaseList from './components/caseList'
-import Layout from './components/Layout'
-import type { Case } from './lib/types';
+import "./App.css";
+import CaseList from "./components/CaseList";
+import Layout from "./components/Layout";
+import type { Case } from "./lib/types";
 
 const cases: Case[] = [
-        {
-          id: "1",
-          caseNumber: "2023가합12345",
-          caseType: "CIVIL",
-          caseName: "대여금",
-          court: "서울중앙지방법원",
-          filedAt: new Date("2023-01-15"),
-          createdAt: new Date("2023-01-10"),
-        }, {
-          id: "2",
-          caseNumber: "2024가합345",
-          caseType: "CRIMINAL",
-          caseName: "절도",
-          court: "서울중앙지방법원",
-          filedAt: new Date("2024-01-15"),
-          createdAt: new Date("2024-01-10"),
-        }];
+	{
+		id: "1",
+		caseNumber: "2023가합12345",
+		caseType: "CIVIL",
+		caseName: "대여금",
+		court: "서울중앙지방법원",
+		filedAt: new Date("2023-01-15"),
+		createdAt: new Date("2023-01-10"),
+	},
+	{
+		id: "2",
+		caseNumber: "2024가합345",
+		caseType: "CRIMINAL",
+		caseName: "절도",
+		court: "서울중앙지방법원",
+		filedAt: new Date("2024-01-15"),
+		createdAt: new Date("2024-01-10"),
+	},
+];
 
 function App() {
-  return (
-    <Layout>
-      <CaseList cases={cases} />
-    </Layout>
-  )
+	return (
+		<Layout>
+			<CaseList cases={cases} />
+		</Layout>
+	);
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "./src"),
-    },
-  },
-})
+	plugins: [
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(import.meta.dirname, "./src"),
+		},
+	},
+	server: {
+		watch: {
+			ignored: ["**/src-tauri/**"],
+		},
+	},
+});
